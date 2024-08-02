d605b3c92538e022b90f57382450b546b1ceb516
fco-aguirre <46607160+fco-aguirre@users.noreply.github.com>
2024-08-02 12:12:58 -0300

    first version of Modis Download in bash

--- Utilities/1_Download_Modis/Download_Modis.py
+++ Utilities/1_Download_Modis/Download_Modis.py
@@ -24,30 +24,97 @@ class Parameter_Modis:
                 break
             linea_file.append(linea)
         
-        self.param_list = linea_file[6]
-        param_text = StringIO(self.param_list)
-        param_ndarray = np.genfromtxt(param_text, dtype="U25,i8,f8,f8,f8,f8,<U6,<U6,<U6,<U6", delimiter="\t")
-        self.param_array = param_ndarray.item(0)
-        self.name_account = self.param_array[0] # Variable publica
-        self.year = self.param_array[1]
-        self.north = self.param_array[2]
-        self.south = self.param_array[3]
-        self.west = self.param_array[4]
-        self.east = self.param_array[5]
-        self.stat_time = self.param_array[6]
-        self.end_time = self.param_array[7]
-        self.format = self.param_array[8]
-        self.proj = self.param_array[9]
+        
+        _init_year_list = linea_file[7] # initial year
+        _init_year_text = StringIO(_init_year_list)
+        _init_year_ndarray = np.genfromtxt(_init_year_text, dtype="U25,i8", delimiter="\t")
+        _init_year_array = _init_year_ndarray.item(0)
+        self.init_year = _init_year_array[1]
+        
+        
+        _final_year_list = linea_file[8]    # final year 
+        _final_year_text = StringIO(_final_year_list)
+        _final_year_ndarray = np.genfromtxt(_final_year_text, dtype="U25,i8", delimiter="\t")
+        _final_year_array = _final_year_ndarray.item(0)
+        self.final_year = _final_year_array[1]
+        
+        _param_list = linea_file[9] # line with the parameters
+        _param_text = StringIO(_param_list)
+        _param_ndarray = np.genfromtxt(_param_text, dtype="U25,f8,f8,f8,f8,<U6,<U6,<U6,<U6", delimiter="\t")
+        _param_array = _param_ndarray.item(0)
+        self.name_account = _param_array[0]
+        #self.year = _param_array[1]
+        self.north = _param_array[1]
+        self.south = _param_array[2]
+        self.west = _param_array[3]
+        self.east = _param_array[4]
+        self.start_time = _param_array[5]
+        #print(self.start_time)
+        self.end_time = _param_array[6]
+        self.format = _param_array[7]
+        self.proj = _param_array[8]
      
         
 if __name__ == "__main__":
     parameter_file = Parameter_Modis('parameters.txt')
-    '''
-    print('perl order.pl -a ' + parameter_file.name_account + ' -c 61 -t "' + parameter_file.stat_time
-          + ', '+ str(parameter_file.year) + ' 00:00:00" -u "' + parameter_file.end_time + ', ' + str(parameter_file.year) + ' 23:59:59" -resample Nearest,Mosaic -format' +
-          parameter_file.format + ' -n ' + str(parameter_file.north) + ' -w ' + str(parameter_file.west) + ' -s ' + str(parameter_file.south) + ' -e ' + str(parameter_file.east) + ' -proj ' +
-          parameter_file.proj + ' -to_do post-process "MOD09GA___sur_refl_b01_1" "MOD09GA___sur_refl_b02_1" "MOD09GA___sur_refl_b03_1" "MOD09GA___sur_refl_b04_1" "MOD09GA___sur_refl_b05_1" "MOD09GA___sur_refl_b06_1" "MOD09GA___sur_refl_b07_1" "MOD09GA___SolarZenith_1"' +
-          ' "MOD09GQ___sur_refl_b01_1" "MOD09GQ___sur_refl_b02_1" "MOD35_L2___Cloud_Mask"')
+    
+    n_years = (parameter_file.final_year - parameter_file.init_year) + 1
+    
+    print(parameter_file.init_year)
+    
+    
+    
+    if n_years == 1:
+        
+        '''
+        print('perl order.pl -a ' + parameter_file.name_account + ' -c 61 -t "' + parameter_file.start_time
+              + ', '+ str(parameter_file.init_year) + ' 00:00:00" -u "' + parameter_file.end_time + ', ' + str(parameter_file.init_year) + ' 23:59:59" -resample Nearest,Mosaic -format' +
+              parameter_file.format + ' -n ' + str(parameter_file.north) + ' -w ' + str(parameter_file.west) + ' -s ' + str(parameter_file.south) + ' -e ' + str(parameter_file.east) + ' -proj ' +
+              parameter_file.proj + ' -to_do post-process "MOD09GA___sur_refl_b01_1" "MOD09GA___sur_refl_b02_1" "MOD09GA___sur_refl_b03_1" "MOD09GA___sur_refl_b04_1" "MOD09GA___sur_refl_b05_1" "MOD09GA___sur_refl_b06_1" "MOD09GA___sur_refl_b07_1" "MOD09GA___SolarZenith_1"' +
+              ' "MOD09GQ___sur_refl_b01_1" "MOD09GQ___sur_refl_b02_1" "MOD35_L2___Cloud_Mask"', shell=True)
+        
+        #print(parameter_file.init_year)
+        
+        '''
+        
+        res = subprocess.call('perl ./Modis_Script/order_MWS.pl -a ' + parameter_file.name_account + ' -c 61 -t "' + parameter_file.start_time
+              + ', '+ str(parameter_file.init_year) + ' 00:00:00" -u "' + parameter_file.end_time + ', ' + str(parameter_file.init_year) + ' 23:59:59" -resample Nearest,Mosaic -format ' +
+              parameter_file.format + ' -n ' + str(parameter_file.north) + ' -w ' + str(parameter_file.west) + ' -s ' + str(parameter_file.south) + ' -e ' + str(parameter_file.east) + ' -proj ' +
+              parameter_file.proj + ' -to_do post-process "MOD09GA___sur_refl_b01_1" "MOD09GA___sur_refl_b02_1" "MOD09GA___sur_refl_b03_1" "MOD09GA___sur_refl_b04_1" "MOD09GA___sur_refl_b05_1" "MOD09GA___sur_refl_b06_1" "MOD09GA___sur_refl_b07_1" "MOD09GA___SolarZenith_1"' +
+              ' "MOD09GQ___sur_refl_b01_1" "MOD09GQ___sur_refl_b02_1" "MOD35_L2___Cloud_Mask"', shell=True)
+        
+        #res = subprocess.call('perl ./Modis_Script/order_MWS.pl -c 61 -t "Aug 1, 2008 00:00:00" -u "Aug 1, 2008 23:59:59" -n 5.0 -w 100.0 -s -10.0 -e 125.0 -to_do list MOD021KM MYD021KM', shell=True)
+        
+        res
+        
+
+        
+    else:
+        print('dos años')
+        
+        
+        years = []
+        #print(shape)
+        
+        
+        for y in range(n_years):
+            years.append(parameter_file.init_year + y)
+        
+        for year in years:
+            #print(i)
+            
+            #res = subprocess.call('perl ./Modis_Script/order_MWS.pl -c 61 -t "Aug 1, ' + str(year) + ' 00:00:00" -u "Aug 1, ' + str(year) + ' 23:59:59" -n 5.0 -w 100.0 -s -10.0 -e 125.0 -to_do list MOD021KM MYD021KM', shell=True)
+            #res
+            
+            res = subprocess.call('perl ./Modis_Script/order_MWS.pl -a ' + parameter_file.name_account + ' -c 61 -t "' + parameter_file.stat_time
+                  + ', '+ str(parameter_file.year) + ' 00:00:00" -u "' + parameter_file.end_time + ', ' + str(parameter_file.year) + ' 23:59:59" -resample Nearest,Mosaic -format ' +
+                  parameter_file.format + ' -n ' + str(parameter_file.north) + ' -w ' + str(parameter_file.west) + ' -s ' + str(parameter_file.south) + ' -e ' + str(parameter_file.east) + ' -proj ' +
+                  parameter_file.proj + ' -to_do post-process "MOD09GA___sur_refl_b01_1" "MOD09GA___sur_refl_b02_1" "MOD09GA___sur_refl_b03_1" "MOD09GA___sur_refl_b04_1" "MOD09GA___sur_refl_b05_1" "MOD09GA___sur_refl_b06_1" "MOD09GA___sur_refl_b07_1" "MOD09GA___SolarZenith_1"' +
+                  ' "MOD09GQ___sur_refl_b01_1" "MOD09GQ___sur_refl_b02_1" "MOD35_L2___Cloud_Mask"', shell=True)
+            
+            res
+        
+        
     '''
     
     res = subprocess.call('perl order.pl -a ' + parameter_file.name_account + ' -c 61 -t "' + parameter_file.stat_time
@@ -57,6 +124,12 @@ if __name__ == "__main__":
           ' "MOD09GQ___sur_refl_b01_1" "MOD09GQ___sur_refl_b02_1" "MOD35_L2___Cloud_Mask"')
     #res = subprocess.call('perl ./Modis_Script/order_MWS.pl -c 61 -t "Aug 1, 2008 00:00:00" -u "Aug 1, 2008 23:59:59" -n 5.0 -w 100.0 -s -10.0 -e 125.0 -to_do list MOD021KM MYD021KM', shell=True)
     res
+   
+    res_1 = subprocess.call('perl ./Modis_Script/order_MWS.pl -c 61 -t "Aug 1, 2008 00:00:00" -u "Aug 1, 2008 23:59:59" -n 5.0 -w 100.0 -s -10.0 -e 125.0 -to_do list MOD021KM MYD021KM', shell=True)
+    res_2 = subprocess.call('perl ./Modis_Script/order_MWS.pl -c 61 -t "Aug 2, 2008 00:00:00" -u "Aug 2, 2008 23:59:59" -n 5.0 -w 100.0 -s -10.0 -e 125.0 -to_do list MOD021KM MYD021KM', shell=True)
     
+    res_1
+    res_2
+    '''
         
         
